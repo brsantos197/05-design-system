94ce78feef9a94266e695edc3b6da350ce1a54ce
Bruno Santos <brsantos197@gmail.com>
2023-06-21 10:48:38 -0300

    teste

--- packages/docs-react-2/src/stories/Avatar.stories.tsx
+++ packages/docs-react-2/src/stories/Avatar.stories.tsx
@@ -2,7 +2,7 @@ import type { StoryObj, Meta } from '@storybook/react'
 import { Avatar, AvatarProps } from '@emerald-ui/react'
 
 export default {
-  title: 'Data display/Avatar',
+  title: 'Data display/Avatar 2',
   component: Avatar,
   args: {
     src: 'https://avatars.githubusercontent.com/u/54981257?v=4',
