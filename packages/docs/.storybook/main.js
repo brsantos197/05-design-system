c885156b18b9346599e80e02fbdf9fc10437987a
Bruno Santos <brsantos197@gmail.com>
2023-06-21 11:53:07 -0300

    fix docs

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/05-design-system/react';
+      config.base = '/05-design-system/';
     }
 
     return config
